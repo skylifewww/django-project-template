280a5376eb7f69eeb404d597c0cc76fdbfbceebe
Stas Vilisov <st@pirsipy.com>
2015-02-19 19:07:30 +0300

    added csrf middleware

--- settings/middleware.py
+++ settings/middleware.py
@@ -3,6 +3,8 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.common.CommonMiddleware',
     'django.middleware.locale.LocaleMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
 )
