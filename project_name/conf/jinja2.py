e9faef6ea8671632fec6049453d86a6cb4873df7
Alexander <my@pinkevich.net>
2015-08-07 17:41:41 +0300

    Update jinja2.py

--- project_name/conf/jinja2.py
+++ project_name/conf/jinja2.py
@@ -2,8 +2,11 @@ from jinja2 import Environment
 
 from django.conf import settings
 from django.contrib.staticfiles.storage import staticfiles_storage
+from django.contrib.messages import get_messages
 from django.core.urlresolvers import reverse
+from django.utils.translation import ugettext
 from django.utils.timezone import now
+from django.template.defaultfilters import timesince, timeuntil
 
 from {{ project_name }}.conf import config
 from {{ project_name }}.core.utils import intspace, set_param
@@ -11,10 +14,12 @@ from {{ project_name }}.core.utils import intspace, set_param
 
 def environment(**options):
     env = Environment(**options)
+    env.filters['timesince'] = timesince
+    env.filters['timeuntil'] = timeuntil
     env.globals.update({
         'dir': dir, 'list': list, 'len': len, 'enumerate': enumerate, 'range': range,
         'settings': settings, 'config': config,
         'now': now, 'intspace': intspace, 'set_param': set_param,
-        'static': staticfiles_storage.url, 'url': reverse,
+        'static': staticfiles_storage.url, 'url': reverse, '_': ugettext, 'messages': get_messages,
     })
     return env
